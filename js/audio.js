884a07e9c1b34d174ce6d9f75b2cd565f0c00726
Olivier Blanc <olivier3lanc@gmail.com>
2021-09-28 22:38:06 +0200

    Remove autoplay

--- js/audio.js
+++ js/audio.js
@@ -1,9 +1,3 @@
 const player = new Plyr('#player', {
     controls: ['play', 'volume'],
 });
-const playerTrig = function(e) {
-    document.removeEventListener('scroll', playerTrig);
-    console.log('kjk');
-    player.play();
-}
-document.addEventListener('scroll', playerTrig);
