ea52e77cecd3dc4896fa02d1eda37b74f0f0c748
Olivier Blanc <olivier3lanc@gmail.com>
2021-10-18 17:28:17 +0200

    WIP iframes

---
+++ js/iframe.js
@@ -0,0 +1,16 @@
+const el_loader = window.parent.document.querySelector('#cdp_loader');
+if (el_loader !== null) {
+    window.addEventListener('load', function() {
+        el_loader.classList.add('u-transparent');
+    });
+    document.querySelectorAll('#cdp_next_scene, #cdp_prev_scene').forEach(function(el) {
+        el.addEventListener('click', function(e) {
+            e.preventDefault();
+            el_loader.classList.remove('u-transparent');
+            const url = e.target.getAttribute('href');
+            setTimeout(function() {
+                window.location.href = url;
+            }, 500);
+        })
+    })
+}
