cdbf0f33a4fbdff2ac3f9ab86318b7ee80cd6d2d
Olivier Blanc <olivier3lanc@gmail.com>
2021-10-21 18:37:11 +0200

    WIP

--- js/casadepapel.js
+++ js/casadepapel.js
@@ -1,52 +1,32 @@
 let casaDePapel = {
     defaults: {
         el_audio_player: document.getElementById('cdp_audio_player'),
-        el_scene_container: document.querySelector('main'),
-        scenes: [
-            'cdp_executives',
-            'cdp_ursula',
-            'cdp_albamiguel',
-            'cdp_alvaro',
-            'cdp_pacopedro',
-            'cdp_migue',
-            'cdp_ykiti',
-            'cdp_beretta',
-            'cdp_cristina',
-            'cdp_esther',
-            'cdp_final'
-        ]
+        el_play_pause_button: document.getElementById('cdp_play_pause'),
+        el_nav_home: document.getElementById('cdp_nav_home'),
+        el_loader: document.getElementById('cdp_loader'),
+        el_scenes: document.getElementById('cdp_scenes'),
     },
-    scenesElements: {},
     playPauseAudio: function() {
-        if (this.defaults.el_audio_player !== null) {
+        if (this.defaults.el_audio_player !== null && this.defaults.el_play_pause_button !== null) {
             if (this.defaults.el_audio_player.paused) {
                 this.defaults.el_audio_player.play();
+                this.defaults.el_play_pause_button.innerHTML = 'Pause';
             } else {
                 this.defaults.el_audio_player.pause();
-            }
-        }
-    },
-    playScene: function(id) {
-        if (Object.keys(this.scenesElements).length > 0 && this.defaults.el_scene_container !== null) {
-            // console.log('1st');
-            if (typeof this.scenesElements[id] == 'object') {
-                // console.log('2nd');
-                const el_current_scene = this.defaults.el_scene_container.querySelector('iframe');
-                if (el_current_scene !== null) {
-                    console.log(el_current_scene);
-                    this.defaults.el_scene_container.removeChild(el_current_scene);
-                }
+                this.defaults.el_play_pause_button.innerHTML = 'Play';
             }
         }
     },
     update: function() {
-        // if (this.defaults.scenes !== undefined && Object.keys(this.scenesElements).length == 0) {
-        //     this.defaults.scenes.forEach(function(scene_id) {
-        //         casaDePapel.scenesElements[scene_id] = document.createElement('iframe');
-        //         casaDePapel.scenesElements[scene_id]['src'] = scene_id+'.html';
-        //         casaDePapel.scenesElements[scene_id].classList.add('c-position', 'm-fixed', 'm-top-0', 'u-w-100', 'u-h-100vh', 'u-brep-no-repeat', 'u-bpos-center', 'u-m-none', 'u-p-none', 'u-b-none');
-        //     });
-        // }
+        if (this.defaults.el_nav_home !== null && this.defaults.el_loader !== null && this.defaults.el_scenes !== null) {
+            this.defaults.el_nav_home.addEventListener('click', function(e) {
+                e.preventDefault();
+                casaDePapel.defaults.el_loader.classList.remove('u-transparent');
+                setTimeout(function() {
+                    casaDePapel.defaults.el_scenes.setAttribute('src', 'cdp_executives.html');
+                }, 500);
+            })
+        }
     }
 }
 casaDePapel.update();
